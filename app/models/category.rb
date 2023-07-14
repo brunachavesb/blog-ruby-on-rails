de4a9ab61fb233b4fbf96202fcfd4248b0a74be5
brunachavesb <brunachavesb@gmail.com>
2023-07-13 22:24:01 -0300

    feat: :sparkles: adicionado validações

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :recipes
+
+  validates :name, presence: true, length: { maximum: 255 }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :category
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :instructions, presence: true
 end
